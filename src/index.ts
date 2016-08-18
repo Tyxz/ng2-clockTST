44976ce6727eb11e689a3a25d6c964873fa1637e
Arne Rantzen <arne@rantzen.net>
2016-08-18 17:26:21 +0200

    Bugfixes
    Day 2
    Run first tests
    Should work

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import {DIRECTIVES} from './directives';
-import {PIPES} from './pipes';
+// import {DIRECTIVES} from './directives';
+// import {PIPES} from './pipes';
 import {PROVIDERS} from './services';
 
-export * from './directives';
+// export * from './directives';
 export * from './services';
-export * from './pipes';
+// export * from './pipes';
 
 export default {
-  directives: [DIRECTIVES],
-  pipes: [PIPES],
+  // directives: [DIRECTIVES],
+  // pipes: [PIPES],
   providers: [PROVIDERS]
 }
